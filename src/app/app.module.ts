2788d6bd37ba4821466761f1e6d63570374f6c72
philipNgungi <philophilo037@gmail.com>
2023-02-06 10:30:10 +0300

    feat:generated a post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
+// import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    PostComponent,
+    // PostComponent
   ],
   imports: [
     BrowserModule,
